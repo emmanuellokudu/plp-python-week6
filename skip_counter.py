c66f563b493a7bf93a3c85dc9e00845ebe440710
emmanuellokudu <emmanuel.lokudu.f@gmail.com>
2026-09-23 12:20:50 +0300

    Refactor the print-funciton inside loop in skip_counter file

--- skip_counter.py
+++ skip_counter.py
@@ -1,9 +1,9 @@
 print("Even numbers:")
 
 for i in range(0, 21, 2):
-    print(f"{i} Even number")
+    print(i)
 
 print("\nCountdown:")
 
 for i in range(10, -1, -1):
-    print(f"{i} countdown")
+    print(i)
