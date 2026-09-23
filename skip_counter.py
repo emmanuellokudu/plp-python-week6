89cd493c327f968ed7575477bd0bdb9494c2c249
emmanuellokudu <emmanuel.lokudu.f@gmail.com>
2026-09-23 12:17:07 +0300

    Refactor the skip_counter file

--- skip_counter.py
+++ skip_counter.py
@@ -1,6 +1,9 @@
-for i in range(0, 21):
-    if i % 2 == 0:
-        print(f"{i} Even number")
-        
+print("Even numbers:")
+
+for i in range(0, 21, 2):
+    print(f"{i} Even number")
+
+print("\nCountdown:")
+
 for i in range(10, -1, -1):
     print(f"{i} countdown")
